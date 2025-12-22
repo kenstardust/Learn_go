e5491163e524c54141709aa9c2bdeb606c11b1a0
梧华 <11917426+stardustknightk@user.noreply.gitee.com>
2025-12-22 20:57:32 +0800

    分组路由

--- Kee/kee.go
+++ Kee/kee.go
@@ -1,37 +1,54 @@
 package Kee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*Context)
 
+/*
+*
+顶层egine也能使用分组功能
+*/
 type Engine struct {
 	router *router
+	*RouterGroup
+	groups []*RouterGroup //保存所有组
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc //中间件
+	parent      *RouterGroup
+	engine      *Engine
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.addRoute(method, pattern, handler)
-}
+//func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+//	engine.router.addRoute(method, pattern, handler)
+//}
 
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+	engine.router.addRoute("GET", pattern, handler)
 }
 
 func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+	engine.router.addRoute("POST", pattern, handler)
 }
 
-func (engine *Engine) PUT(method string, pattern string, handler HandlerFunc) {
-	engine.addRoute("PUT", pattern, handler)
+func (engine *Engine) PUT(pattern string, handler HandlerFunc) {
+	engine.router.addRoute("PUT", pattern, handler)
 }
 
 func (engine *Engine) DEL(pattern string, handler HandlerFunc) {
-	engine.addRoute("DELETE", pattern, handler)
+	engine.router.addRoute("DELETE", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
@@ -41,4 +58,37 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("add route method:%4s pattern:%s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (group *RouterGroup) PUT(pattern string, handler HandlerFunc) {
+	group.addRoute("PUT", pattern, handler)
+}
+
+func (group *RouterGroup) DELETE(pattern string, handler HandlerFunc) {
+	group.addRoute("DELETE", pattern, handler)
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"Kee"
-	"fmt"
 	"net/http"
 )
 
@@ -10,16 +9,30 @@ func main() {
 
 	r := Kee.New()
 
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *Kee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello World</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/hello", func(c *Kee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello World</h1>")
+		})
+	}
 
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *Kee.Context) {
+			c.String(http.StatusOK, "Hello %s! your path is %s", c.Param("name"), c.Path)
+		})
+	}
+
+	r.POST("/login", func(c *Kee.Context) {
+		c.JSON(http.StatusOK, Kee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
 	r.Run(":9999")
 
 }
